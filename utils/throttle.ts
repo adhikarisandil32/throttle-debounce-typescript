aca6949257774d671c70ca19afa196077d06f7cb
Sandil Adhikari <adhikarisandil32@gmail.com>
2024-07-22 23:53:47 +0545

    throttle documentation

--- utils/throttle.ts
+++ utils/throttle.ts
@@ -4,13 +4,15 @@ const throttle = (cb: Function, delay: number) => {
 
   return (...args: []) => {
     const now = Date.now()
-    let workingArgs: []
+    let workingArgs: [] //this variable to store all the arguments so that we can use it to later call the function when user stops typing
 
     if (now - lastCall >= delay) {
+      // once its more than the delay, we call the function
       cb(...args)
       workingArgs = []
       lastCall = now
     } else {
+      // if its not we record its arguments and at the very end, when user stops typing we call the function again
       workingArgs = [...args]
 
       clearTimeout(timeout)
