13f041b7b4c496660860f716623dece50a568cea
Sandil Adhikari <adhikarisandil32@gmail.com>
2024-07-22 23:50:21 +0545

    updates-to-throttle function

--- utils/throttle.ts
+++ utils/throttle.ts
@@ -1,12 +1,24 @@
 const throttle = (cb: Function, delay: number) => {
   let lastCall: number = 0
+  let timeout: ReturnType<typeof setTimeout>
 
   return (...args: []) => {
     const now = Date.now()
+    let workingArgs: []
 
     if (now - lastCall >= delay) {
       cb(...args)
+      workingArgs = []
       lastCall = now
+    } else {
+      workingArgs = [...args]
+
+      clearTimeout(timeout)
+
+      timeout = setTimeout(() => {
+        cb(...workingArgs)
+        workingArgs = []
+      }, delay)
     }
   }
 }
